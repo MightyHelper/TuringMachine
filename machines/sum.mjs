d7a7316990c748db6102fd0040cda22b78975119
Federico Williamson <federicowilliamson@hotmail.co.uk>
2022-10-04 19:05:37 -0300

    Finish Sum

--- machines/sum.mjs
+++ machines/sum.mjs
@@ -1,31 +1,14 @@
 let deltamap={
-	'Init_1':'Rem_2_>',
-	'Rem_1':'Rem_1_>',
-	'Rem_&':'Del_&_>',
-	'Del_2':'Del_2_>',
-	'Del_1':'Get_2_<',
-	'Get_2':'Get_2_<',
-	'Get_&':'Fet_&_<',
-	'Fet_1':'Fet_1_<',
-	'Fet_2':'Mov_2_>',
-	'Mov_1':'Rem_2_>',
-	'Mov_&':'Delete2_#_<', //first is longer
-	'Del_#':'Delete1_*_<', //second is longer
-	'Delete2_1':'Delete2_*_<',
-	'Delete2_2':'Delete2_*_<',
-	'Delete2_#':'MoveBack2_*_>',
-	'MoveBack2_*':'MoveBack2_*_>',
-	'MoveBack2_#':'FC2_#_>',
-	'FC2_2':'FC2_1_>',
-	'FC2_#':'FC2_#_-',
-	'FC2_1':'FC2_1_<',
-	'Delete1_2':'Delete1_*_<',
-	'Delete1_&':'Convert_#_<',
-	'Convert_2':'Convert_1_<',
-	'Convert_#':'Convert_#_-',
+	'Init_1':'Del#_#_<',
+	'Del#_#':'Del#_*_>',
+	'Del#_1':'End_#_>',
+	'Del#_&':'End_#_-',
+	'End_1':'End_1_>',
+	'End_&':'End_1_>',
+	'End_#':'End_#_-',
 }
 window.addEventListener('DOMContentLoaded', ()=>{
-	window.createTuringMachine("max", 1, "1111&111", deltamap)
-	window.createTuringMachine("max", 1, "1111&111111", deltamap)
-	window.createTuringMachine("max", 1, "1&1", deltamap)
+	window.createTuringMachine("sum", 1, "1111&1", deltamap)
+	window.createTuringMachine("sum", 1, "1&111", deltamap)
+	window.createTuringMachine("sum", 1, "1&1", deltamap)
 })
